707edb2890fb96a3846c75f85a0847204872ac02
Ailén Grimaldi <63558201+ail3ngrimaldi@users.noreply.github.com>
2025-12-18 17:12:39 -0300

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import logo from "@/assets/logo.svg";
+import logo from "@/assets/losburritosdedulcinea.svg";
 
 const HeroSection = () => {
   const scrollToProducts = () => {
