d56fa3203c7ee27869c50be01990697e6a7fb23f
Ailén Grimaldi <63558201+ail3ngrimaldi@users.noreply.github.com>
2025-12-26 23:10:21 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
+        fredoka: ['Fredoka', 'sans-serif'],
         display: ['Fredoka', 'sans-serif'],
       },
       colors: {
